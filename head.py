47f79a4a8037fee483bda49dea76abfe1222c156
hattiffnat <sereyfeam@gmail.com>
2017-08-27 20:16:26 +0300

    first step

--- head.py
+++ head.py
@@ -12,20 +12,45 @@ class Character(ShowBase):
 	SCALE = 0.07
 	POS = (5, 0, 0.4)
 	HPR = (-90, 0, 0)
+	RATE = 1.5
 
 	def __init__(self, path):
 
+		#======ANIMATION LIST=====================
 		self.char = Actor(path+'male.egg', {
-			'walk' : path+'male-male_walk.egg',
+			'walk_loop' : path+'male-walk_loop.egg',
+			'walk_start' : path+'male-walk_start.egg'
 			})
+
+		#======SCALE POSITION ROTATE APPLY========
 		self.char.setScale(self.SCALE, self.SCALE, self.SCALE)
 		self.char.setPos(self.POS)
 		self.char.setHpr(self.HPR)
-		
+
+		#======INPUT ANIMATIONS VARIABLES=========
+		self.start = self.char.getAnimControl('walk_start')
+		self.loop = self.char.getAnimControl('walk_loop')
+
+		#======ANIMATION PLAY RATE SET============
+		self.start.setPlayRate(self.RATE)
+		self.loop.setPlayRate(self.RATE)
+
+		#======RENDER APPLY=======================
 		self.char.reparentTo(render)
 
-	
+	def walk(self):
+		if self.start.getPlayRate() < 0:
+			self.start.setPlayRate(self.RATE)
+		self.start.play()
 
+	def loopwalk(self):
+		if not(self.start.isPlaying() or self.loop.isPlaying()):
+			self.loop.play()
+
+	def stopwalk(self):
+		self.start.setPlayRate(self.RATE * (-1))
+		self.start.play()
+		
 
 class Window(ShowBase):
 
@@ -34,7 +59,9 @@ class Window(ShowBase):
 		self.loadEnv(arenapath)
 		self.char01 = Character(charpath)
 
-		self.accept('w-down', self.char01.walk)
+		self.accept('w', self.char01.walk)
+		self.accept('w-repeat', self.char01.loopwalk)
+		self.accept('w-up', self.char01.stopwalk)
 
 	def loadEnv(self, arenapath):
 		self.scene = loader.loadModel(arenapath)
