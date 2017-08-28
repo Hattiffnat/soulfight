c7b7edeafe6cb420b58888d1f56666b8cacfd586
hattiffnat <sereyfeam@gmail.com>
2017-08-28 20:46:41 +0300

    second step

--- test.py
+++ test.py
@@ -1,67 +1,100 @@
 from math import pi, sin, cos
 
+#import direct.directbase.DirectStart
+
+from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Point3
  
+class Character(ShowBase):
+
+	SCALE = 1
+	POS = (0, 0, 0.4)
+	HPR = (-90, 0, 0)
+	RATE = 1.5
+
+	def __init__(self, path):
+
+		#======ANIMATION LIST=====================
+		self.char = Actor(path+'male.egg', {
+			'walk_loop' : path+'male-walk_loop.egg',
+			'walk_start' : path+'male-walk_start.egg'
+			})
+
+		#======SCALE POSITION ROTATE SET==========
+		self.char.setScale(self.SCALE, self.SCALE, self.SCALE)
+		self.char.setPos(self.POS)
+		self.char.setHpr(self.HPR)
+
+		#======INPUT ANIMATIONS VARIABLES=========
+		self.start = self.char.getAnimControl('walk_start')
+		self.loop = self.char.getAnimControl('walk_loop')
+
+		#======ANIMATION PLAY RATE SET============
+		self.start.setPlayRate(self.RATE)
+		self.loop.setPlayRate(self.RATE)
+
+		#======RENDER ON==========================
+		self.char.reparentTo(render)
+
+	def walk(self):
+		if self.start.getPlayRate() < 0:
+			self.start.setPlayRate(self.RATE)
+		self.start.play()
+
+	def loopwalk(self):
+		if not(self.start.isPlaying() or self.loop.isPlaying()):
+			self.loop.play()
+
+	def stopwalk(self):
+		self.start.setPlayRate(self.RATE * (-1))
+		self.start.play()
+		
+class Environment(ShowBase):
+
+	SCALE = 2
+	POS = (0, 0, 0)
+	HPR = (0, 0, 0)
+	RATE = 1
+
+	def __init__(self, path):
+		#======LOAD MODEL=========================
+		self.map = loader.loadModel(path)
+		
+		#======SCALE POSITION ROTATE SET==========
+		self.map.setScale(self.SCALE, self.SCALE, self.SCALE)
+		self.map.setPos(self.POS)
+		self.map.setHpr(self.HPR)
+
+		#======RENDER ON==========================
+		self.map.reparentTo(render)
+
+
 class Window(ShowBase):
-	def __init__(self):
+
+	def __init__(self, arenapath, charpath):
+
 		ShowBase.__init__(self)
 
-		self.loadModels()
-		self.loadActor()
-		self.ActorPos()
+		self.env = Environment(arenapath)
 
-		#self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+		self.char01 = Character(charpath)
 
-	def loadModels(self):
+		self.plight = PointLight('pl1')
+		self.plight.setColor(VBase4(1, 1, 1, 1))
+		self.lamp = render.attachNewNode(self.plight)
+		self.lamp.setPos(0, 0, 10)
+		render.setLight(self.lamp)
 
-		self.scene = loader.loadModel("models/arenas/arena2.egg")
-		self.scene.reparentTo(render)
-		self.scene.setScale(2, 2, 2)
-		self.scene.setPos(0, 0, 0)
-
-	def loadActor(self):
-		self.actor = Actor("models/human/male.egg",
-			{"bend1" : "models/human/male-male_walk.egg",
-			# "bend2" : "models/testworm-ArmatureAction.001.egg",
-			 #"bend3" : "models/testworm-ArmatureAction.002.egg",
-			 #"bend4" : "models/testworm-wormbend01.001.egg"
-			 })
-		self.actor.reparentTo(render)
-		self.actor.loop("bend1")
-
-	def ActorPos(self):
-		PosInterval1 = self.actor.posInterval(13,
-												Point3(0, -10, 0),
-												startPos=Point3(0, 10, 0))
-		PosInterval2 = self.actor.posInterval(13,
-												Point3(0, 10, 0),
-												startPos=Point3(0, 10, 0))
-		HrpInterval1 = self.actor.hprInterval(3,
-												Point3(180, 0, 0),
-												startHpr=Point3(0, 0, 0))
-		HrpInterval2 = self.actor.hprInterval(3,
-												Point3(0, 0, 0),
-												startHpr=Point3(180, 0, 0))	
-
-		self.pandaPace = Sequence(PosInterval1,
-									HrpInterval1,
-									PosInterval2,
-									HrpInterval2,
-									name="wormPace")
-		self.pandaPace.loop()
-
-	def spinCameraTask(self, task):
-		angleDeg = task.time * 6
-		angleRad = angleDeg * (pi / 180.0)
-		self.camera.setPos(20*sin(angleRad), -20*cos(angleRad), 10)
-
-		self.camera.setHpr(angleDeg, -45, 0)
-		return Task.cont
+		self.accept('w', self.char01.walk)
+		self.accept('w-repeat', self.char01.loopwalk)
+		self.accept('w-up', self.char01.stopwalk)
 
 
-app = Window()
+location = r'models/arenas/arena3/arena3.egg'
+char = r'models/human/'
+
+app = Window(location, char)
 app.run()
