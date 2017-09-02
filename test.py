0bc3d88c71bbae04912e2df2334c7a4af5cd7b6a
hattiffnat <sereyfeam@gmail.com>
2017-09-02 16:22:41 +0300

    sixth step

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
-#from direct.interval.IntervalGlobal import Sequence
+from direct.interval.ActorInterval import ActorInterval
+from direct.interval.IntervalGlobal import Sequence
 
 class Character(ShowBase):
 
@@ -20,8 +21,10 @@ class Character(ShowBase):
 
 		#======ANIMATION LIST=====================
 		self.char = Actor(path+'male.egg', {
-			'walk_loop' : path+'male-walk_loop.egg',
-			'walk_start' : path+'male-walk_start.egg'
+			'walk_loop' : path+'male-male_walk.egg',
+			'walk_start_l' : path+'male-start_walk_l.egg',
+			'walk_start_r' : path+'male-start_walk_r.egg',
+			'swap' : path+'male-swap_wearpon.egg'
 			})
 
 		#======SCALE POSITION ROTATE SET==========
@@ -29,29 +32,74 @@ class Character(ShowBase):
 		self.char.setPos(self.POS)
 		self.char.setHpr(self.HPR)
 
-		#======INPUT ANIMATIONS VARIABLES=========
-		self.start = self.char.getAnimControl('walk_start')
-		self.loop = self.char.getAnimControl('walk_loop')
+		#======MAKING SUBPARTS====================
+		self.char.makeSubpart('legs', 
+			[
+			'upperleg_l',
+			'upperleg_r',
+			'lowerleg_l',
+			'lowerleg_r',
+			'legk_l',
+			'legk_r',
+			'foot_l',
+			'foot_r',
+			'knee_l',
+			'knee_r'
+			])
+		#self.char.makeSubpart('tors', 
+		#	[
+		#	'stomach',
+		#	'chest',
+		#	])
+		self.char.makeSubpart('left_hand', 
+			[
+			'shoulder_l',
+			'elbow_l',
+			'armk_l'
+			])
+		self.char.makeSubpart('right_hand', 
+			[
+			'shoulder_r',
+			'elbow_r',
+			'armk_r'
+			])
+		self.char.makeSubpart('head', 
+			[
+			'head',
+			])
 
+		#======INPUT ANIMATIONS INTERVALS=========
+		self.start_l = self.char.actorInterval(
+			'walk_start_l',
+			)
+		
 		#======ANIMATION PLAY RATE SET============
-		self.start.setPlayRate(self.RATE)
-		self.loop.setPlayRate(self.RATE)
+		self.char.setPlayRate(self.RATE, ['walk_loop', 'walk_start'])
 
 		#======RENDER ON==========================
 		self.char.reparentTo(render)
+		print(Actor.listJoints(self.char))
 
-	def walk(self):
-		if self.start.getPlayRate() < 0:
-			self.start.setPlayRate(self.RATE)
-		self.start.play()
+	def startwalk(self):
+		if self.char.getPlayRate('walk_start_l') < 0:
+			self.char.setPlayRate(self.RATE, 'walk_start_l')
+		self.char.play('walk_start_l', partName='legs')
 
 	def loopwalk(self):
-		if not(self.start.isPlaying() or self.loop.isPlaying()):
-			self.loop.play()
+		print(self.char.getCurrentAnim())
+		anim = self.char.getCurrentAnim()
+		if anim != 'walk_start_l' and anim != 'walk_loop':
+			self.char.play('walk_loop', partName='legs')
 
 	def stopwalk(self):
-		self.start.setPlayRate(self.RATE * (-1))
-		self.start.play()
+		self.char.setPlayRate(-self.RATE, 'start_walk_l')
+		self.char.play('start_walk_l', partName='legs')
+
+	def swap_left(self):
+		self.char.play('swap', partName='left_hand')
+
+	def swap_right(self):
+		self.char.play('swap', partName='right_hand')
 		
 class Environment(ShowBase):
 
@@ -118,9 +166,11 @@ class Window(ShowBase):
 
 		self.char01 = Character(charpath)
 
-		self.accept('w', self.char01.walk)
+		self.accept('w', self.char01.startwalk)
 		self.accept('w-repeat', self.char01.loopwalk)
 		self.accept('w-up', self.char01.stopwalk)
+		self.accept('z', self.char01.swap_left)
+		self.accept('c', self.char01.swap_right)
 
 arenapath = r'models/arenas/arena3/'
 charpath = r'models/human/'
